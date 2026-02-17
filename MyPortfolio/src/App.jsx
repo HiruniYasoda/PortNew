617f366d402ff7e1bce13e4f72f3b362a24f28e8
hiruni sethmini <hirunisethmini@gmail.com>
2026-02-18 01:04:22 +0530

    scroll bar

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-7l'>hello world </div>
+    <div className='text-7xl h-[1400px]'>hello world </div>
   )
 }
